4a8f08c4c7e77eb2bcbb2032945ee2cc8c7fa1cf
Mohammed <dev.mohammedsh@gmail.com>
2024-10-07 06:45:08 +0530

    🗄️ DB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
 
+dotenv.config()
 
 const app = express()
 
 app.listen(5000, () => {
+    connectDB()
     console.log(`Server is running on http://localhost:5000`);
 })
