5175c1d7ba1423f36c93821cc415635cc9b38336
Mohammed <dev.mohammedsh@gmail.com>
2024-10-07 15:32:55 +0530

    ✨ Add navbar with theme toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,55 @@
+import {
+	Button,
+	Container,
+	Flex,
+	HStack,
+	Text,
+	useColorMode,
+} from "@chakra-ui/react"
+import { Moon, Plus, Sun } from "lucide-react"
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
-	return <div>Navbar</div>
+	const { colorMode, toggleColorMode } = useColorMode()
+
+	return (
+		<Container maxW={"1140px"} px={4}>
+			<Flex
+				h={16}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				flexDir={{
+					base: "column",
+					sm: "row",
+				}}
+			>
+				<Text
+					fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+				</Text>
+
+				<HStack spacing={2} alignItems={"center"}>
+					<Link to={"/create"}>
+						<Button>
+							<Plus size={20} />
+						</Button>
+					</Link>
+					<Button onClick={toggleColorMode}>
+						{colorMode === "light" ? (
+							<Moon size={20} />
+						) : (
+							<Sun size={20} />
+						)}
+					</Button>
+				</HStack>
+			</Flex>
+		</Container>
+	)
 }
 export default Navbar
